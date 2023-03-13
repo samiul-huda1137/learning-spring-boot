8dbce2eea8d1ae8232740415be51d45e112cd9b1
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-03-13 17:51:12 +0600

    java objects to json objects

--- demo/src/main/java/com/spring/Main.java
+++ demo/src/main/java/com/spring/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -15,7 +17,18 @@ public class Main {
 
     @GetMapping("/")
     public GreetResponse greet(){
-        return new GreetResponse("this is a json object");
+        GreetResponse response = new GreetResponse("this is a json object",
+                List.of("Java", "Python", "Flutter", "Spring"),
+                new Person("Huda",24,10000)
+        );
+        return  response;
     }
-    record GreetResponse(String greet){}
+    record Person(String name,int age, double savings){
+
+    }
+    record GreetResponse(
+            String greet,
+            List<String> programmingLanguages,
+            Person person
+    ){}
 }
