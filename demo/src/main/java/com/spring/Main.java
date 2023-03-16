911432bdd616368dd54ee36cf4310253bd0ae9ef
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-03-16 22:02:22 +0600

    read customers Api

--- demo/src/main/java/com/spring/Main.java
+++ demo/src/main/java/com/spring/Main.java
@@ -3,32 +3,28 @@ package com.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customer")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
 
         SpringApplication.run(Main.class, args);
     }
-
-    @GetMapping("/")
-    public GreetResponse greet(){
-        GreetResponse response = new GreetResponse("this is a json object",
-                List.of("Java", "Python", "Flutter", "Spring"),
-                new Person("Huda",24,10000)
-        );
-        return  response;
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
     }
-    record Person(String name,int age, double savings){
-
-    }
-    record GreetResponse(
-            String greet,
-            List<String> programmingLanguages,
-            Person person
-    ){}
 }
